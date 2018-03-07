bb78c9fc440476a562f98725980261571a37a31d
richardyam1 <richardyam1@yahoo.com>
2018-03-07 11:58:08 -0500

    Give JButtons variable names

--- Calculator/src/com/company/UserInterface.java
+++ Calculator/src/com/company/UserInterface.java
@@ -29,34 +29,72 @@ public class UserInterface implements Runnable {
 
     private void createComponents(Container container){
         JPanel numberTextField = new JPanel();
-        GridLayout calculatorLayout = new GridLayout(5,3,30,20);
+        JTextField display = new JTextField();
+        //display.setEditable(false);
+        GridLayout calculatorLayout = new GridLayout(5,4,30,20);
+
         JPanel numberRow1 = new JPanel();
         JPanel numberRow2 = new JPanel();
         JPanel numberRow3 = new JPanel();
         JPanel numberRow4 = new JPanel();
+        JButton back = new JButton("\u2190");
+        JButton clear = new JButton("C");
+        JButton plus = new JButton("+");
+        JButton multi = new JButton("*");
+        JButton divide = new JButton("/");
+        JButton minus = new JButton("-");
+        JButton num0 = new JButton("0");
+        num0.setPreferredSize(new Dimension(87,26));
+        JButton num1 = new JButton("1");
+        JButton num2 = new JButton("2");
+        JButton num3 = new JButton("3");
+        JButton num4 = new JButton("4");
+        JButton num5 = new JButton("5");
+        JButton num6 = new JButton("6");
+        JButton num7 = new JButton("7");
+        JButton num8 = new JButton("8");
+        JButton num9 = new JButton("9");
+        JButton decimal = new JButton(".");
+        JButton equal = new JButton("=");
+        //equal.setPreferredSize(new Dimension(60,60));
+
         numberTextField.setLayout(new BorderLayout());
-        numberTextField.add(new JTextField(), BorderLayout.SOUTH);
-        createButtonRow(numberRow1, 7, 9);
-        createButtonRow(numberRow2, 4,6);
-        createButtonRow(numberRow3, 1, 3);
+        numberTextField.add(display, BorderLayout.SOUTH);
 
         container.add(numberTextField, BorderLayout.PAGE_START);
+
         container.setLayout(calculatorLayout);
 
-        JButton zero = new JButton("0");
-        zero.setPreferredSize(new Dimension(87,26));
-        numberRow4.add(zero);
-        numberRow4.add(new JButton("."));
+        numberRow1.add(num7);
+        numberRow1.add(num8);
+        numberRow1.add(num9);
 
-        numberRow1.add(new JButton("/"));
-        numberRow2.add(new JButton("*"));
-        numberRow3.add(new JButton("-"));
-        numberRow4.add(new JButton("+"));
+        numberRow2.add(num4);
+        numberRow2.add(num5);
+        numberRow2.add(num6);
+
+        numberRow3.add(num1);
+        numberRow3.add(num2);
+        numberRow3.add(num3);
+
+        numberRow4.add(num0);
+        numberRow4.add(decimal);
+        numberRow4.add(equal);
+
+        numberRow1.add(back);
+        numberRow1.add(clear);
+        numberRow2.add(plus);
+        numberRow2.add(multi);
+        numberRow3.add(divide);
+        numberRow3.add(minus);
+
 
         container.add(numberRow1);
         container.add(numberRow2);
         container.add(numberRow3);
         container.add(numberRow4);
+
+
 
     }
 
@@ -64,11 +102,6 @@ public class UserInterface implements Runnable {
         return frame;
     }
 
-    public void createButtonRow(JPanel row, int first, int last){
-        for(int i = first; i <= last; i++){
-            row.add(new JButton(i + ""));
-        }
-    }
 
 
 
